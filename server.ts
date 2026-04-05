ae1c951ad134ed247153036969e501eddc6c7545
MarushiniB <marushinibabu2203@gmail.com>
2026-04-05 15:18:03 +0530

    Added package

--- server.ts
+++ server.ts
@@ -25,6 +25,9 @@ app.get("/", (_, res) => {
   res.send("API Running...");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on ${process.env.PORT}`);
+// Use PORT from environment or fallback to 5000
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
